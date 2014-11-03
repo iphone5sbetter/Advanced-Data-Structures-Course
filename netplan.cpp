c024b767d046fd19ae694c338b8f88d8a2ad3d13
Kyle Barron-Kraus <kylebarronkraus@gmail.com>
2014-11-03 00:19:05 -0800

    Added starter code

---
+++ netplan.cpp
@@ -0,0 +1,40 @@
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <string>
+
+#include "UndirectedGraph.hpp"
+
+/**
+ * Entry point into the netplan program.
+ *
+ * -Reads a file from the filesystem according to the specification for
+ *  PA3, creating an UndirectedGraph.
+ * -Finds the total cost & ping time of the graph as presented in the input
+ *  file.
+ * -Determines the minimum cost graph from the original graph.
+ * -Finds the total cost & ping time of the minimum cost graph.
+ * -Finds the change of cost & ping time from the original graph to the
+ *  minimum cost graph.
+ * -Prints the results to stdout.
+ *
+ * Usage:
+ *   ./netplan infile
+ *
+ */
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " infile" << std::endl;
+        return EXIT_FAILURE;
+    }
+    
+    std::ifstream in(argv[1]);
+    if (!in) {
+        std::cerr << "Unable to open file for reading." << std::endl;
+        return EXIT_FAILURE;
+    }
+    
+    // Implementation here
+
+    return EXIT_SUCCESS;
+}
